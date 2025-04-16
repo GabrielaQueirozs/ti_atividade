7bff7a829bdcac97ec2b6bca84c340050398378b
Gabriela Queiroz <162500638+GabrielaQueirozs@users.noreply.github.com>
2025-04-16 16:46:28 -0300

    Add files via upload

--- preload.js
+++ preload.js
@@ -5,6 +5,7 @@
 // importaçao dos recursos do framework
 // contextbridge segunrança | ipcrenderer comunicação
 const {contextBridge,ipcRenderer} = require('electron')
+const cliente = require('./src/models/cliente')
 
 // Enviar ao main um pedido para conexão com o banco de dados e troca do icone no processo de renderização(index.html)
 ipcRenderer.send('db-connect')
@@ -12,7 +13,9 @@ ipcRenderer.send('db-connect')
 contextBridge.exposeInMainWorld('api',{
     clientWindow: () => ipcRenderer.send('client-Window'),
     osWindow: () => ipcRenderer.send('os-Window'),
-    dbStatus:(message)=>ipcRenderer.on('db-status', message)
+    dbStatus:(message)=>ipcRenderer.on('db-status', message),
+    newCliente: (client) => ipcRenderer.send('new-Client', client),
+    resetForm: 
 })
 
 function dbStatus(message){
